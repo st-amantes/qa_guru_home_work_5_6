a884d7f25599ae60dae8eacec2522608d52c951e
Vitaliy Eremeev <evit.viter@gmail.com>
2023-04-23 15:34:35 +0700

    1 passed 2 passed 3 passed 4 passed

--- def_passed.py
+++ def_passed.py
@@ -82,13 +82,17 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 def open_browser(browser_name):
-    actual_result = (open_browser.__name__.replace('_','').title() + f'[{browser_name}]')
+    actual_result = (open_browser.__name__.replace('_',' ').title() + f' [{browser_name}]')
+    print(end='\n')
+    print(actual_result, end='\n')
     assert actual_result == "Open Browser [Chrome]"
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = go_to_companyname_homepage.__name__.replace("_", " ").title() + f" [{page_url}]"
+    print(actual_result, end='\n')
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = find_registration_button_on_login_page.__name__.replace("_", " ").title() + f" [{page_url}, {button_text}]"
+    print(actual_result, end='\n')
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
